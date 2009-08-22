1aa522e0f66f5dedb323a067158527011845ff93
Jesse Andrews <anotherjesse@gmail.com>
2009-08-21 19:32:36 -0700

    more work on session stuff

--- server/sso.py
+++ server/sso.py
@@ -3,12 +3,14 @@ class LazyUser(object):
         if not hasattr(request, '_cached_user'):
             from django.contrib.auth.models import User, AnonymousUser
             try:
-                user_id = request.COOKIES['sso']
+                user_id = request.COOKIES['sso'].split('|')[0]
                 # TODO: verify signature
                 # TODO: move the user object if it doesn't exist in the local DB
                 user = User.objects.get(id__exact=request.COOKIES['sso']) or AnonymousUser()
             except KeyError:
                 user = AnonymousUser()
+            # TODO: if session already had an SSO_USER and it is different
+            #       then a new session should be created
             request._cached_user = user
         return request._cached_user
 
--- server/views.py
+++ server/views.py
@@ -7,7 +7,7 @@ def login(request):
     if request.POST:
         user = User.objects.get(username__exact=request.POST['username']) # TODO: check password as well ;)
         response = HttpResponseRedirect('/')
-        response.set_cookie('sso', str(user.id)) # TODO: sign the cookie
+        response.set_cookie('sso', str(user.id) + '|SIGNATURE') # TODO: really sign it
         return response
 
     return render_to_response('login.html', {})
