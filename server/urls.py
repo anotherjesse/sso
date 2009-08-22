6c2cea6bff3ceff5f6f012f022f1b167e59e15ba
Jesse Andrews <anotherjesse@gmail.com>
2009-08-21 18:13:14 -0700

    login kinda works

--- server/urls.py
+++ server/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', 'server.views.status'),
     (r'^login/$', 'django.contrib.auth.views.login'),
     (r'^logout/$', 'server.views.logout'),
     (r'^admin/(.*)', admin.site.root),
--- server/views.py
+++ server/views.py
@@ -4,3 +4,7 @@ import django.contrib.auth
 def logout(request, next_page=None):
     django.contrib.auth.logout(request)
     return HttpResponseRedirect(next_page or '/')
+
+def status(request):
+    return HttpResponse('hello: %s' % request.user)
+    
